6ec74b0023cd5046061ee96433c6901b080bef0a
mattiamonti <mattiamonti2001@gmail.com>
2025-03-10 15:47:15 +0100

    discount fixed

--- components/Pricing.tsx
+++ components/Pricing.tsx
@@ -8,6 +8,7 @@ const pricingPlans = [
   {
     title: "Collaboratore",
     price: "Gratis",
+    discountedPrice: "",
     features: [
       "Profilo gratuito",
       "Scopri progetti su cui collaborare",
@@ -17,6 +18,7 @@ const pricingPlans = [
   {
     title: "Collaboratore +",
     price: "€3,99/mese",
+    discountedPrice: "",
     features: [
       "Profilo premium",
       "Posizione prioritaria nei risultati",
@@ -26,6 +28,7 @@ const pricingPlans = [
   {
     title: "Owner",
     price: "€4,99/mese",
+    discountedPrice: "Gratis per 3 mesi!",
     features: [
       "Pubblica progetti",
       "Scopri talenti con cui collaborare",
@@ -43,7 +46,14 @@ export default function Pricing() {
       <div className="mt-8 grid md:grid-cols-3 gap-6">
         {pricingPlans.map((plan, index) => (
         <ProfileCard title={plan.title} key={index}>
-            <p className="text-2xl text-gray-800 font-bold text-center mb-6">{plan.price}</p>
+            {(plan.discountedPrice) ? (
+                <>
+                <p className="text-md text-gray-500 font-semibold text-center line-through"> {plan.price} </p>
+                <p className="text-2xl text-gray-800 font-bold text-center mb-6 text-lime-700">{plan.discountedPrice}</p>
+                </>
+            ) : (
+                <p className="text-2xl text-gray-800 font-bold text-center mb-6">{plan.price}</p>
+            )}
             <Separator className="my-2"/>
               {plan.features.map((feature, index) => (
                 <div key={index} className="flex items-center gap-2 text-gray-800">
