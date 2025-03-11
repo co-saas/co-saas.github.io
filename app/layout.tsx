b4ae5e9ea20a2261dd5ecdcde370f164bc7e67e0
cosaas <cosaas.saas@gmail.com>
2025-03-11 10:15:25 +0100

    added ga

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { GoogleAnalytics } from '@next/third-parties/google';
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -76,6 +77,7 @@ export default function RootLayout({
       >
         {children}
       </body>
+      <GoogleAnalytics gaId="G-7C3BF621FL" />
     </html>
   );
 }
